33b56ab46a42b68ee4192d4abe21e8b2a0caf39c
Sanix Darker <sanix000@yahoo.fr>
2017-08-27 13:56:11 +0200

    First commit

---
+++ config.h
@@ -0,0 +1,13 @@
+
+#ifndef _CONFIG_H
+#define _CONFIG_H
+
+#define PROJECT_NAME "KeyLy"
+#define PROJECT_VERSION_MAJOR 1
+#define PROJECT_VERSION_MINOR 1
+#define PROJECT_VERSION_PATCH 0
+#define PROJECT_COPYRIGHT "Copyright (C) 2017 Sanix Darker"
+#define FILEEXT ".log"
+#define DEBUG
+
+#endif /* _CONFIG_H */
